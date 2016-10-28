e78bbfeb226c5f970166d8171e457fba2c5c8de9
Adrien Agnel <adrien.agnel@gmail.com>
2016-10-28 10:27:28 +0200

    created project

---
+++ calculator.py
@@ -0,0 +1,54 @@
+import unittest
+import numpy
+import itertools
+
+class Calculator:
+
+    def add(self, input=None):
+        if input:
+            input = input.split('//')
+            input = [x[1:].split(x[0]) for x in input if len(x)>0]
+            input = itertools.chain(*input)
+            input = [x.split('\n') for x in input]
+            input = itertools.chain(*input)
+            input = list(input)
+            print(input)
+            tot = 0
+            for element in input:
+                tot += int(element)
+            return tot
+        else:
+            return 0
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_noEntry(self):
+        self.assertEqual(0,self.calc.add())
+
+    def test_emptyEntry(self):
+        self.assertEqual(0,self.calc.add(""))
+
+    def test_oneEntry(self):
+        self.assertEqual(1,self.calc.add("//,\n1"))
+
+    def test_twoEntry(self):
+        self.assertEqual(13,self.calc.add("//,\n4,9"))
+
+    def test_unknownEntry(self):
+        n = numpy.random.randint(3,99)
+        numbersArray = numpy.random.randint(0,10,n)
+        tot = numbersArray.sum()
+        #test
+        self.assertEqual(tot, self.calc.add(','.join(map(str,numbersArray))))
+
+    def test_lineBreak(self):
+        self.assertEqual(12, self.calc.add("//,\n2\n4,2\n1,3"))
+
+    def test_separators(self):
+        self.assertEqual(21, self.calc.add("//,\n2\n4,2\n1,3//;\n3;4\n2"))
+
+if __name__ == '__main__':
+    unittest.main()
